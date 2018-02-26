1f5fa2c0fa0fc549aff5b15fdbda9ffa5fc81805
fabienogli <fabien.ogli@etu.univ-lyon1.fr>
2018-02-26 09:50:47 +0100

    new files

--- src/Server/Connexion.java
+++ src/Server/Connexion.java
@@ -1,4 +1,39 @@
 package Server;
 
-public class Connexion {
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
+
+public class Connexion implements Runnable{
+    private InetAddress clientAdress;
+    private int portEcoute;
+    private Socket clientSocket;
+    private int clientPort = 0;
+
+
+    public Connexion(InetAddress clientAdress, Socket clientSocket, int clientPort) {
+        this.clientAdress = clientAdress;
+        this.clientSocket = clientSocket;
+        this.clientPort = clientPort;
+    }
+
+
+    @Override
+    public void run() {
+        System.out.println("Dans le run de sHTTP");
+        /**
+         * Creation du flux in et out
+         */
+        DataInputStream inFromClient;
+        DataOutputStream outToClient;
+        //Dans le run de serveur
+        try {
+            inFromClient = new DataInputStream(clientSocket.getInputStream());
+            outToClient = new DataOutputStream(clientSocket.getOutputStream());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/Server/Server.java
+++ src/Server/Server.java
@@ -1,4 +1,47 @@
 package Server;
 
+import java.io.IOException;
+import java.net.*;
+
 public class Server {
+    final static int BUF_SIZE = 1024;
+    public  int portEcoute;
+    private ServerSocket serverSocket;
+
+    public Server(){
+        portEcoute = 2026;
+        try {
+            serverSocket = new ServerSocket(portEcoute);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void lancer(){
+
+        byte [] buffer = new byte[BUF_SIZE];
+        try {
+            while (true)
+            {
+                Socket clientSocket = serverSocket.accept();
+                DatagramPacket receivePacket = new DatagramPacket(buffer, buffer.length);
+                int portClient = receivePacket.getPort();
+                InetAddress address = receivePacket.getAddress();
+                Connexion serveurHTTP = new Connexion(address,clientSocket, portClient);
+                Thread thread = new Thread(serveurHTTP);
+                System.out.println("Lancement du serveur");
+                thread.start();
+            }
+        } catch (SocketException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Server server = new Server();
+        server.lancer();
+    }
 }
