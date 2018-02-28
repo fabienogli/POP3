6658c4f30c7ae7abcfbe8b4052b0c4424c40e2dd
Mark Kpamy <markkpamy@gmail.com>
2018-02-28 08:37:12 +0100

    modifs

--- src/Server/Commande.java
+++ src/Server/Commande.java
@@ -1,7 +1,11 @@
 package Server;
 
+import java.io.*;
+
 public class Commande {
 
+    private final int i_USER = 0;
+    private final int i_PASS = 1;
     public static String quit() {
 
         return null;
@@ -68,6 +72,45 @@ public class Commande {
             return false;
         }
         return false;
+    }
+    public String getDataFromDb(int i_data) {
+        String toReturn = "";
+        try {
+            String chemin = "./storage.csv";
+            File file = new File(chemin);
+            FileReader fileReader = new FileReader("./storage.csv");
+            BufferedReader db = new BufferedReader(fileReader);
+            String chaine;
+            int i = 1;
+            while((chaine = db.readLine())!= null)
+            {
+                if(i > 1)
+                {
+                    String[] tabChaine = chaine.split(",");
+                    //Tu effectues tes traitements avec les données contenues dans le tableau
+                    //La première information se trouve à l'indice 0
+                    for (int x = 0; x < tabChaine.length; x++) {
+                        if (x == i_USER) {
+                            System.out.println("user = " + tabChaine[x]+"\n");
+                        } else if (x == i_PASS) {
+                            System.out.println("pass = " + tabChaine[x]+"\n");
+                        } else {
+                            System.out.println("message = " + tabChaine[x]+"\n");
+                        }
+                    }
+                }
+                i++;
+            }
+            db.close();
+
+        }
+        catch (FileNotFoundException e)
+        {
+            System.out.println("Le fichier est introuvable !");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return toReturn;
     }
 
 }
--- src/Server/Connexion.java
+++ src/Server/Connexion.java
@@ -101,45 +101,7 @@ public class Connexion implements Runnable {
 
 
 
-    public String getDataFromDb(int i_data) {
-        String toReturn = "";
-        try {
-            String chemin = "./storage.csv";
-            File file = new File(chemin);
-            FileReader fileReader = new FileReader("./storage.csv");
-            BufferedReader db = new BufferedReader(fileReader);
-            String chaine;
-            int i = 1;
-            while((chaine = db.readLine())!= null)
-            {
-                if(i > 1)
-                {
-                    String[] tabChaine = chaine.split(",");
-                    //Tu effectues tes traitements avec les données contenues dans le tableau
-                    //La première information se trouve à l'indice 0
-                    for (int x = 0; x < tabChaine.length; x++) {
-                        if (x == i_USER) {
-                            System.out.println("user = " + tabChaine[x]+"\n");
-                        } else if (x == i_PASS) {
-                            System.out.println("pass = " + tabChaine[x]+"\n");
-                        } else {
-                            System.out.println("message = " + tabChaine[x]+"\n");
-                        }
-                    }
-                }
-                i++;
-            }
-            db.close();
 
-        }
-        catch (FileNotFoundException e)
-        {
-            System.out.println("Le fichier est introuvable !");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return toReturn;
-    }
 
     public String getUSER() {
         return USER;
