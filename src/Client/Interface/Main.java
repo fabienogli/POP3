c43b1ead08a973893af85d38db53d05150d27ad6
Unknown <lombardoflorian@gmail.com>
2018-03-05 20:29:40 +0100

    recup

--- src/Client/Interface/Main.java
+++ src/Client/Interface/Main.java
@@ -1,27 +1,68 @@
 package Client.Interface;
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
 
 import java.awt.*;
+import java.io.IOException;
 
 public class Main extends Application {
 
+    private Stage primaryStage;
+    private BorderPane rootLayout;
+
+
+
     @Override
     public void start(Stage primaryStage) throws Exception{
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("Client POP3");
+
         Parent root = FXMLLoader.load(getClass().getResource("vueClient.fxml"));
-        primaryStage.setTitle("Client POP3");
+        this.primaryStage.setScene(new Scene(root, 560, 390));
+        this.primaryStage.show();
+
+
+
+        /*primaryStage.setTitle("Client POP3");
         primaryStage.setScene(new Scene(root, 560, 390));
-        primaryStage.show();
+        primaryStage.show();*/
 
 
     }
 
+
+    public void initRootLayout() {
+        // Load root layout from fxml file.
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(Main.class.getResource("vueClient.fxml"));
+        try {
+            rootLayout = (BorderPane) loader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // Show the scene containing the root layout.
+        Scene scene = new Scene(rootLayout);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public Stage getPrimaryStage() {
+        return primaryStage;
+    }
 
     public static void main(String[] args) {
         launch(args);
     }
+
+
+
+    private ObservableList<> objetADefinir = FXCollections.observableArrayList();
 }
